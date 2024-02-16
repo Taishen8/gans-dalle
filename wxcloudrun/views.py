6cf03984236efcfd37aa481bfb659f5a914111be
Taishen8 <hpgan08@foxmail.com>
2024-02-16 22:38:17 +0800

    Add laughing

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from wxcloudrun.models import Counters
 
 
-logger = logging.getLogger('log')
+logger = logging.getLogger("log")
 
 
 def index(request, _):
@@ -16,7 +16,7 @@ def index(request, _):
      `` request `` 请求对象
     """
 
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def counter(request, _):
@@ -26,15 +26,19 @@ def counter(request, _):
      `` request `` 请求对象
     """
 
-    rsp = JsonResponse({'code': 0, 'errorMsg': ''}, json_dumps_params={'ensure_ascii': False})
-    if request.method == 'GET' or request.method == 'get':
+    rsp = JsonResponse(
+        {"code": 0, "errorMsg": ""}, json_dumps_params={"ensure_ascii": False}
+    )
+    if request.method == "GET" or request.method == "get":
         rsp = get_count()
-    elif request.method == 'POST' or request.method == 'post':
+    elif request.method == "POST" or request.method == "post":
         rsp = update_count(request)
     else:
-        rsp = JsonResponse({'code': -1, 'errorMsg': '请求方式错误'},
-                            json_dumps_params={'ensure_ascii': False})
-    logger.info('response result: {}'.format(rsp.content.decode('utf-8')))
+        rsp = JsonResponse(
+            {"code": -1, "errorMsg": "请求方式错误"},
+            json_dumps_params={"ensure_ascii": False},
+        )
+    logger.info("response result: {}".format(rsp.content.decode("utf-8")))
     return rsp
 
 
@@ -46,10 +50,12 @@ def get_count():
     try:
         data = Counters.objects.get(id=1)
     except Counters.DoesNotExist:
-        return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
-    return JsonResponse({'code': 0, 'data': data.count},
-                        json_dumps_params={'ensure_ascii': False})
+        return JsonResponse(
+            {"code": 0, "data": 0}, json_dumps_params={"ensure_ascii": False}
+        )
+    return JsonResponse(
+        {"code": 0, "data": data.count}, json_dumps_params={"ensure_ascii": False}
+    )
 
 
 def update_count(request):
@@ -59,16 +65,18 @@ def update_count(request):
     `` request `` 请求对象
     """
 
-    logger.info('update_count req: {}'.format(request.body))
+    logger.info("update_count req: {}".format(request.body))
 
-    body_unicode = request.body.decode('utf-8')
+    body_unicode = request.body.decode("utf-8")
     body = json.loads(body_unicode)
 
-    if 'action' not in body:
-        return JsonResponse({'code': -1, 'errorMsg': '缺少action参数'},
-                            json_dumps_params={'ensure_ascii': False})
+    if "action" not in body:
+        return JsonResponse(
+            {"code": -1, "errorMsg": "缺少action参数"},
+            json_dumps_params={"ensure_ascii": False},
+        )
 
-    if body['action'] == 'inc':
+    if body["action"] == "inc":
         try:
             data = Counters.objects.get(id=1)
         except Counters.DoesNotExist:
@@ -76,16 +84,24 @@ def update_count(request):
         data.id = 1
         data.count += 1
         data.save()
-        return JsonResponse({'code': 0, "data": data.count},
-                    json_dumps_params={'ensure_ascii': False})
-    elif body['action'] == 'clear':
+        return JsonResponse(
+            {"code": 0, "data": data.count}, json_dumps_params={"ensure_ascii": False}
+        )
+    elif body["action"] == "clear":
         try:
             data = Counters.objects.get(id=1)
             data.delete()
         except Counters.DoesNotExist:
-            logger.info('record not exist')
-        return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
+            logger.info("record not exist")
+        return JsonResponse(
+            {"code": 0, "data": 0}, json_dumps_params={"ensure_ascii": False}
+        )
+    elif body["action"] == "laugh":
+        return JsonResponse(
+            {"code": 0, "data": "114514"}, json_dumps_params={"ensure_ascii": False}
+        )
     else:
-        return JsonResponse({'code': -1, 'errorMsg': 'action参数错误'},
-                    json_dumps_params={'ensure_ascii': False})
+        return JsonResponse(
+            {"code": -1, "errorMsg": "action参数错误"},
+            json_dumps_params={"ensure_ascii": False},
+        )
